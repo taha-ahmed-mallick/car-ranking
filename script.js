d4773c442593cbbf07db0605d129ad5103d6250a
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-03-31 20:27:28 +0500

    list items can be added or deleted; icons for dark and light theme.

--- script.js
+++ script.js
@@ -2,30 +2,46 @@ let cars = [];
 let input = document.getElementById("car-name");
 let submitBtn = document.getElementById("enter");
 let list = document.getElementsByTagName("ul")[0];
+let icon = document.getElementById("icon");
+
+window.onload = () => {
+	window.matchMedia("(prefers-color-scheme: dark)").matches
+		? (icon.href = "./icon-dark.png")
+		: (icon.href = "./icon.png");
+};
 
 input.addEventListener("keydown", (eve) => {
 	if ((eve.key == "Enter" || eve.keyCode == 13) && input.value != "") {
-		appendList();
+		cars.push(input.value);
+		input.value = "";
+		displayList();
 	}
 });
 
 submitBtn.addEventListener("click", () => {
 	if (input.value != "") {
-		appendList();
+		cars.push(input.value);
+		input.value = "";
+		displayList();
 	}
 });
 
-function appendList() {
-	cars.push(input.value);
-	input.value = "";
-	const listItem = `
+function displayList() {
+	list.innerHTML = "";
+	for (let i = 0; i < cars.length; i++) {
+		list.innerHTML += `
 	<li draggable="true">
 		<div>
 			<div>
-				<span>${cars[cars.length - 1]}</span>
-				<span class="material-symbols-outlined"> delete </span>
+				<span>${cars[i]}</span>
+				<span class="material-symbols-outlined" onclick="del(${i})"> delete </span>
 			</div>
 		</div>
 	</li>`;
-	list.innerHTML += listItem;
+	}
+}
+
+function del(index) {
+	cars.splice(index, 1);
+	displayList();
 }
